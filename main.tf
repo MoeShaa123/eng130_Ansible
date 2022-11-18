2addd62d4ca65b11ff5571ff0a107d1521b2d16d
unknown <mohamedyusuf01@hotmail.com>
2022-11-18 17:58:03 +0000

    update

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ provider "aws" {
 }
 
 
-
 resource "aws_vpc" "main-vpc" {
   cidr_block       = var.vpc-id
   instance_tenancy = "default"
@@ -43,14 +42,23 @@ resource "aws_route_table" "main-route-table" {
 }
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.main-vpc.id
-  cidr_block = var.subnet-id
+  cidr_block = var.subnet1-id
   availability_zone = "eu-west-1a"
 
   tags = {
-    Name = "eng130_yusuf_subnet"
+    Name = "eng130_yusuf_subnet1"
   }
 }
 
+resource "aws_subnet" "subnet-2" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = var.subnet2-id
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "eng130_yusuf_subnet2"
+  }
+}
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.main-route-table.id
@@ -94,26 +102,56 @@ ingress {
   }
 }
 
-resource "aws_instance" "app_instance" {
-  ami                         = var.ami-id
-  key_name = "eng130"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
-  subnet_id =  aws_subnet.subnet-1.id
-  tags = {
-    Name = "eng130-yusuf-terraform-app"
-  }
+# resource "aws_instance" "app_instance" {
+#   ami                         = var.ami-id
+#   key_name = "eng130-new"
+#   instance_type               = "t2.micro"
+#   associate_public_ip_address = true
+#   vpc_security_group_ids = [aws_security_group.allow_web.id]
+#   subnet_id =  aws_subnet.subnet-1.id
+#   tags = {
+#     Name = "eng130-yusuf-terraform-app"
+#   }
   
+# }
+
+resource "aws_launch_template" "tf-launch-template"{
+    name = "eng130_yusuf_lt"
+    image_id = var.ami-id
+    instance_type = "t2.micro"
+    key_name = "eng130-new"
+    vpc_security_group_ids = [aws_security_group.allow_web.id] # Don't use this one, Create a new one.
+    # user_data = "${base64encode(data.template_file.user_data_lw.rendered)}"
+    tags = {
+        Name = "eng130_yusuf_lt"
+    }
 }
-# what type of server with what sort of function
+resource "aws_autoscaling_group" "tf-asg"{
+    name = "eng130_yusuf_terraform_asg"
+    desired_capacity = 2
+    max_size = 3
+    min_size = 2
+    vpc_zone_identifier = [aws_subnet.subnet-1.id]
 
-# add resource
+    tag {
+      key = "Name"
+      propagate_at_launch = true
+      value = "eng130_yusuf_terraform"
+    }
 
-# ami
+    launch_template {
+      id = aws_launch_template.tf-launch-template.id 
+    }
+}
 
-# instance type
+resource "aws_lb" "app-lb" {
+  name = "eng130-yusuf-terraform-lb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.allow_web.id]
+  subnets = [
+    aws_subnet.subnet-1.id,
+    aws_subnet.subnet-2.id
+  ]
+}
 
-# do we need public ip or not
-
-# name the server
--- variable.tf
+++ variable.tf
@@ -4,9 +4,14 @@ variable "ami-id" {
 	default = "ami-0b47105e3d7fc023e"
 } 
 
-variable "subnet-id" {
+variable "subnet1-id" {
 	default = "10.0.11.0/24"
 } 
+
+variable "subnet2-id" {
+	default = "10.0.24.0/24"
+} 
+
 
 variable "vpc-id" {
 	default = "10.0.0.0/16"
